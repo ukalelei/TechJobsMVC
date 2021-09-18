511d4bda72fd820e156a24339d5a70c85dc44804
ukalelei <75921243+ukalelei@users.noreply.github.com>
2021-09-17 19:07:21 -0500

    Add action method Results that takes two parameters and post results to route "/search/results" once form is submitted

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -14,6 +14,7 @@ namespace TechJobsMVC.Controllers
     {
         // renders the form defined in the Views/Search/Index.cshtml template
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
@@ -22,14 +23,28 @@ namespace TechJobsMVC.Controllers
 
         // TODO #3: Create an action method to process a search request and render the updated search view.
 
+        [HttpPost]
+        [Route("/search/results")]
         public IActionResult Results(string searchType, string searchTerm)
         {
+            ViewBag.columns = ListController.ColumnChoices;
+
             List<Job> jobs; //store results in job list
-            if (searchTerm == "")
+            if (String.IsNullOrEmpty(searchTerm))
             {
+                jobs = JobData.FindAll();
+                ViewBag.title = "All Jobs";
+
+            } else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.title = "Jobs with " + ListController.ColumnChoices[searchType] + ": " + searchTerm;
 
             }
 
+            ViewBag.jobs = jobs;
+
+            return View();
         }
     }
 }
